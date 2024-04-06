2261175682c31f6bc344ba40b0cc1032efeea77b
Sorina <91940937+Sorinaaa@users.noreply.github.com>
2024-04-06 22:52:55 +0300

    Add files via upload

---
+++ src/Controller/sorinapage.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class sorinapage extends AbstractController
+{
+    #[Route('/sorina', name: 'sorina')]
+    public function index(): Response
+    {
+        return $this->render('sorina/index.html.twig', [
+            'controller_name' => 'sorinapage',
+        ]);
+    }
+}
